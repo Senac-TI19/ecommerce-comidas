f5930388481f5403ab503e79e3f23f4026815bac
Matheus <matheus.malmeida7@gmail.com>
2021-10-26 23:43:58 -0300

    atualizando serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,6 +2,8 @@ const cacheName = 'ecommerce-matheus-v1';
 
 const recursosCacheados = [
     "./",
+    "./assets/css/style.css",
+    "./manifest.json",
     "./index.html",
     "./index.css",
     "./favicon.ico",
@@ -46,6 +48,23 @@ self.addEventListener("install", function (event) {
         })
     );
 });
+
+self.addEventListener("fetch", function(event){
+    console.log(`Request para o recurso ${event.request.url}`);
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            if(response){
+                console.log(`Recurso encontrado no cache: ${event.request.url}`);
+                return response;
+            }else{
+                console.log(
+                    `Recurso não encontrado no cache. Fazendo request para ${event.request.url}`
+                );
+                return fetch(event.request);
+            }
+        })
+    )
+})
 
 self.addEventListener("activate", function () {
     console.log("Service worker ativado!");
